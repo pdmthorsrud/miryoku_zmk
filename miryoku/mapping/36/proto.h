6280d39585722fccdf2bc1109a13221c305e95bd
Sagii <51820995+pdmthorsrud@users.noreply.github.com>
2025-06-30 01:24:28 +0200

    added mapping 36

--- miryoku/mapping/36/proto.h
+++ miryoku/mapping/36/proto.h
@@ -1,9 +1,9 @@
 // Copyright 2022 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
-#if !defined (MIRYOKU_LAYOUTMAPPING_PROTO)
+#if !defined (MIRYOKU_LAYOUTMAPPING_PROTO_36)
 
-#define MIRYOKU_LAYOUTMAPPING_PROTO( \
+#define MIRYOKU_LAYOUTMAPPING_PROTO_36( \
 K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
 K10, K11, K12, K13, K14,      K15, K16, K17, K18, K19, \
 K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
@@ -16,4 +16,4 @@ K20  K21  K22  K23  K24       K25  K26  K27  K28  K29 \
 
 #endif
 
-#define MIRYOKU_MAPPING MIRYOKU_LAYOUTMAPPING_PROTO
+#define MIRYOKU_MAPPING MIRYOKU_LAYOUTMAPPING_PROTO_36p
